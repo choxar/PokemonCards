d5393a470b58750699816e65015733e10dd1aedd
choxar <elinazambere@yahoo.com>
2021-02-15 17:01:09 +0200

    Done Project

---
+++ PokemonCards/Controller/PokeyTableViewCell.swift
@@ -0,0 +1,43 @@
+
+//  PokemonCards
+//
+//  Created by elina.zambere on 10/02/2021.
+//
+
+import UIKit
+
+class PokeyTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var pokeyImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var subtypeLabel: UILabel!
+    @IBOutlet weak var cardValueLabel: UILabel!
+    @IBOutlet weak var supertypeLabel: UILabel!
+    
+    func setUI(with: Pokemon){
+        
+        nameLabel.text = "Name: " + with.name
+        cardValueLabel.text = "Card Value: " + with.number
+        if let subtype = with.subtype {
+            subtypeLabel.text = "Card:  " + subtype
+        }
+        
+        if let supertype = with.supertype {
+            supertypeLabel.text = supertype
+            if with.supertype == "Trainer" {
+                contentView.backgroundColor = .systemGreen
+            }else{
+                contentView.backgroundColor = .orange
+            }
+        }
+        
+        ImageController.getImage(for: with.imageUrl ?? "", comletion: { image in
+            self.pokeyImageView.image = image
+        })
+        
+        
+        
+    }
+    
+}
